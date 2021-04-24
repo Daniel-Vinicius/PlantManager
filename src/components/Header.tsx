731fb9c3d4b4ad8df4888e8d04864f38026a4dac
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-04-24 19:02:32 -0300

    Imagem Trocando

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
 
-import { View, Text, Image, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableHighlight,
+  Alert,
+} from "react-native";
 
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as ImagePicker from "expo-image-picker";
 
 import userImg from "../assets/daniel.jpg";
 
@@ -17,6 +25,27 @@ interface HeaderProps {
 
 export function Header({ title, subtitle }: HeaderProps) {
   const [userName, setUserName] = useState<string>();
+  const [image, setImage] = useState("");
+
+  async function uploadImage() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync(
+      true
+    );
+    if (status !== "granted") {
+      Alert.alert("É preciso aceitar as permissões para trocar a foto. 😥");
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsMultipleSelection: false,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  }
 
   useEffect(() => {
     async function loadStorageUserName() {
@@ -33,7 +62,12 @@ export function Header({ title, subtitle }: HeaderProps) {
         <Text style={styles.greeting}>{title ? title : "Olá,"}</Text>
         <Text style={styles.userName}>{subtitle ? subtitle : userName}</Text>
       </View>
-      <Image source={userImg} style={styles.image} />
+      <TouchableHighlight onPress={uploadImage} style={styles.image}>
+        <Image
+          source={{ uri: image ? image : "../assets/daniel.jpg" }}
+          style={styles.image}
+        />
+      </TouchableHighlight>
     </View>
   );
 }
